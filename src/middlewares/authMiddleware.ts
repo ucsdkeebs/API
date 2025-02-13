a41fb17e598be67757712d862e68df25290379c5
cwang04 <cjwang2004@gmail.com>
2025-02-12 16:29:35 -0800

    middleware for firebase added

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from 'express';
+import admin from '../config/firebaseConfig';
+import UserModel from '../models/userModel';
+  
+const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const token = req.headers.authorization?.split(' ')[1];
+    if (!token) return res.status(401).json({ error: 'Unauthorized Login' });
+  
+    //verifies if it is a valid firebase token
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    const userId = decodedToken.uid;
+
+    //check if this is a returning or new user
+    const existingUser = await UserModel.findOne({ uid: userId });
+    
+    // if its a new user send them to a page to fill out their information
+    if (!existingUser) {
+      return res.status(200).json({ redirect: '/new-user-registration'});
+    }
+
+    req.user = existingUser;
+    next();
+  } catch (error) {
+    res.status(403).json({ error: 'Invalid token' });
+  }
+};
+  
+export default verifyToken;
+  
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import { createUser, getAllUsers } from '../controllers/userController';
+import verifyToken from '../middlewares/authMiddleware';
 
 const router = Router();
 
 router.post('/create-user', createUser);
+
 router.get('/get-users', getAllUsers);
+
+router.get('/verify', verifyToken, (req, res) => {
+    res.json({ message: "Secure data", user: req.user });
+  });
 
 
 export default router;
