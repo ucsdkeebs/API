c139130be7d4829bfb6ea9a445475763cbf6f60a
Angela Hu <angelahu925@gmail.com>
2025-04-01 17:42:45 -0700

    Trying to fix typescript

--- src/tests/userTests.ts
+++ src/tests/userTests.ts
@@ -1,6 +1,8 @@
-const chai = require('chai')
-const mocha = require('mocha')
-const app = require('../app')
+import * as chai from 'chai';
+import mocha from 'mocha';
+import app from '../app';
 
 const expect = chai.expect;
-const apiURL = 'http://localhost:5000'; //this is the url until we change it
+const apiURL = 'http://localhost:5000'; // This is the URL until we change it
+
+export { expect, apiURL };
