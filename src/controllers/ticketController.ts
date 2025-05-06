de303f956010594e7a8dfa8e8158e11f0d214e7b
Angela Hu <angelahu925@gmail.com>
2025-05-05 17:01:22 -0700

    Added user tickets

--- src/controllers/ticketController.ts
+++ src/controllers/ticketController.ts
@@ -74,3 +74,23 @@ export const getAllTickets = async (req: Request, res: Response) => {
     res.status(500).json({ error: 'Error fetching tickets' });
   }
 };
+
+// Get ticket based on user id
+export const getUserTickets = async (req: Request, res: Response) => {
+  const { userId } = req.params;
+
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    const tickets = await Ticket.find({ ownerId: userId })
+      .populate({ path: "eventId", select: "name" })
+      .populate({ path: "ownerId", select: "email" });
+
+    res.status(200).json({ tickets });
+  } catch (error) {
+    res.status(500).json({ error: 'Error retrieving tickets' });
+  }
+};
--- src/routes/ticketRoutes.ts
+++ src/routes/ticketRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { checkInTicket, createTicket, getAllTickets} from '../controllers/ticketController';
+import { checkInTicket, createTicket, getAllTickets, getUserTickets} from '../controllers/ticketController';
 
 const router = Router();
 
 router.post('/create', createTicket);
 router.post('/check-in/:ticketId/:adminId', checkInTicket);
 router.get("/all/:adminId", getAllTickets);
+router.get("/:userId", getUserTickets);
 
 export default router;
