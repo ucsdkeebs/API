88b2b7d2e53f892e67164f8992cebb7e2d639b7f
cwang04 <cjwang2004@gmail.com>
2026-05-20 21:31:40 -0700

    updating api url

--- src/routes/ticketTailorRoutes.ts
+++ src/routes/ticketTailorRoutes.ts
@@ -4,7 +4,7 @@ import requireAdmin from '../middlewares/adminMiddleware'
 
 const router = Router();
 
-router.post('/update-winner/:ticketId', requireAdmin, updateWinner);
+router.post('/update-winner/:ticketTailorId', requireAdmin, updateWinner);
 router.get('/get-checked-in-no-win', requireAdmin, getCheckedInNoWinTickets);
 router.get('/get-checked-in', requireAdmin, getCheckedInTickets);
 router.get('/get-checked-in-by-raffle', requireAdmin, getCheckedInByRaffleSlot);
